4f56e9b1edf4a7202fb190aa347c71bac28575b9
urarthur <urarthur@gmail.com>
2024-11-22 02:52:44 +0100

    modified:   website/app.py

--- website/app.py
+++ website/app.py
@@ -254,4 +254,4 @@ def get_unit_upgrades():
         return jsonify([])
 
 #if __name__ == "__main__":
- #app.run(host='0.0.0.0', port=5000, debug=True)
+  #app.run(host='0.0.0.0', port=5000, debug=True)
